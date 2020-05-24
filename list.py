5ded5037f7ae2b5c1c5eb666fb4a0a732b746ab3
glondhe <ganesh.londhe@crenexa.com>
2020-05-24 20:11:27 +0530

    list intro with adding and removing element

--- list.py
+++ list.py
@@ -3,5 +3,26 @@ list_1 = ['supriya', 'Manoj', 'Ganesh']
 capitalize_first = list_1[0]
 capitalize_first = capitalize_first.title()
 
-print(capitalize_first)
-print("Length of the given list is ", len(list_1))
+# capitalize first letter
+# print(capitalize_first)
+
+# check the lenth of given string using len()
+# print("Length of the given list is ", len(list_1))
+
+# appending element to the list
+# list_1.append('Pandurang')
+# print(list_1)
+
+# inserting element at particular position
+# list_1.insert(2, 'Pandurang')
+# print(list_1)
+
+# deleting an element from the list
+
+# remove()
+list_1.remove('Ganesh')
+print(list_1)
+
+# pop()
+deleted_element = list_1.pop(1)
+print(deleted_element)
