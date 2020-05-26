c5f83b767b2139e01b69bad462def1d532d109f7
glondhe <ganesh.londhe@crenexa.com>
2020-05-26 09:40:31 +0530

    taking input from user

---
+++ input.py
@@ -0,0 +1,9 @@
+# taking input from user
+
+name = input('Please enter your name > ')
+
+print("Hi ", name)
+
+family_name = input('Enter your family name ')
+
+print("Thanks", name, family_name, "for confirming.")
