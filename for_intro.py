830765774a1d1fc31adade1428e81acd7832ec40
glondhe <ganesh.londhe@crenexa.com>
2020-05-26 10:17:45 +0530

    for intro

---
+++ for_intro.py
@@ -0,0 +1,14 @@
+# for loop introduction
+
+for i in range(0, 10):
+    print(i)
+
+my_list = [2, 4, 5, 6, 8]
+
+for i in my_list:
+    print(i)
+
+my_string = 'my_name_is_Ganesh'
+
+for i in my_string:
+    print(i)
