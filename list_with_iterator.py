6f93d95a9a0b27e8e509b1579eadb5d343f14d24
glondhe <ganesh.londhe@crenexa.com>
2020-05-27 23:10:23 +0530

    list_with_iterator

---
+++ list_with_iterator.py
@@ -0,0 +1,16 @@
+# Following program illustrates the list with iterators
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+
+# reversing a given string
+print(my_list[::-1])
+
+# print all elements in the list
+# for i in my_list:
+#     print(i)
+
+# know the length of the list
+# print(len(my_list))
+
+
+# accessing element by index
+# print(my_list[10])
