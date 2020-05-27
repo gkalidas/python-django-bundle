f024836920a98a944f3af0151f9e042853d27a0a
glondhe <ganesh.londhe@crenexa.com>
2020-05-27 23:28:29 +0530

    list with split

---
+++ list_split.py
@@ -0,0 +1,11 @@
+# list with split
+
+my_string = 'This-is random-sentence'
+
+# convert to string using manual separator
+sentence = my_string.split('-')
+print(sentence)
+
+# converts string into list, default uses space as a separator
+# sentence = my_string.split()
+# print(sentence)
