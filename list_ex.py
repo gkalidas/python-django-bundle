a6d29f70c544298622c6278fba17ebd7e4e7c583
glondhe <ganesh.londhe@crenexa.com>
2020-05-28 12:38:33 +0530

    list with examples"

---
+++ list_ex.py
@@ -0,0 +1,20 @@
+# programmar
+# my_list = list(range(1,11))
+
+# pro
+# my_list = [i for i in range(0,11)]
+# print(my_list)
+
+my_list = ['John', 'Charles', 'Johny']
+
+for name in my_list[:4]:
+    print(name)
+
+# print sum of all elements from the list
+# print(sum(my_list))
+
+# print the max element from list
+# print(max(my_list))
+
+# print the min element from list
+# print(min(my_list))
