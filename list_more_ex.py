f01d8c40a3e3651cc7f006b0410dfa3bd9d29143
glondhe <ganesh.londhe@crenexa.com>
2020-05-28 13:55:46 +0530

    list with more examples

---
+++ list_more_ex.py
@@ -0,0 +1,14 @@
+
+rec = [i for i in range(0, 11)]
+
+my_list = list(map(abs, [1, -9, 8, -6, 7, 8]))
+
+print(my_list)
+
+# string to list
+# a = list('34')
+# print(a)
+
+# appending something to each element before printing
+# for i in a:
+#     print(i, end = ',')
