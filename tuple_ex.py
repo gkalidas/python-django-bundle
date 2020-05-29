781c2fc496d9dc2e10190c205e5749ac5ca2c1dc
glondhe <ganesh.londhe@crenexa.com>
2020-05-29 13:09:48 +0530

    tuples with example and properties

---
+++ tuple_ex.py
@@ -0,0 +1,10 @@
+# tuple with example
+
+first_name = ('Pandurage', 'Priyanka', 'Manoj', 'Supriya')
+last_name = ('Carri', 'Cena', 'Wajpeyi', 'Shinde')
+my_list = []
+
+my_list.append(first_name)
+my_list.append(last_name)
+
+print(my_list)
