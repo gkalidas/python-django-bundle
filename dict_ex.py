a8191f8a58984e7860a5bc92211539e0284a2a74
glondhe <ganesh.londhe@crenexa.com>
2020-05-29 15:35:21 +0530

    dictionary with examples

---
+++ dict_ex.py
@@ -0,0 +1,15 @@
+test = {'name': 'Ganesh', 'age': 24}
+
+print(test)
+
+# adding element to the dictionary
+test['speed'] = 80
+print(test)
+
+# deleting element from the list
+del test['age']
+print(test)
+
+# overriding elements in dictionary
+test['speed'] = 90
+print(test)
