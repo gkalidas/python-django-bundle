0eb705c255f776bbe8e1ff675a8fd5187269c4ac
glondhe <ganesh.londhe@crenexa.com>
2020-06-02 22:44:41 +0530

    python with multiple args

---
+++ args_ex.py
@@ -0,0 +1,16 @@
+def function(*args):
+    for string in args:
+        print(string)
+
+
+function('Hi', 'we', 'are', 'working' ' with ' ' multiple arguments here.')
+
+
+def function(*args):
+    name = args[0]
+    last_name = args[1]
+    age = args[2]
+    print('Welcome ', name, age, last_name)
+
+
+function('John', 'Doe', 45)
