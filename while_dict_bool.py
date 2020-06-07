859e703689511988a41a8fa31b4bbb44f740f162
glondhe <ganesh.londhe@crenexa.com>
2020-06-07 20:25:18 +0530

    while with break

--- while_dict_bool.py
+++ while_dict_bool.py
@@ -1,13 +1,30 @@
 # while with dictionary and boolean
 
-users = ['hai', 'mana', '**', 'gyan', 'Yaha']
-activated_users = []
+# 3
+# while True:
+#     print('please enter you name')
+#     name = input(': ')
+#     if name =='exit' or name == 'e':
+#         break
 
-while users:
-    current_user = users.pop()
-    activated_users.append(current_user)
 
-for i in activated_users:
-    print(i)
-    # always put a title to look it more appealing
-    print(i.title())
+# 2
+# pets = ['cat', 'dog', 'cat', 'paliv', 'prani', 'aahe', 'cat', ]
+
+# while 'cat' in pets:
+#     # removes a 'cat' element from a list
+#     pets.remove('cat')
+# print(pets)
+
+# 1
+# users = ['hai', 'mana', '**', 'gyan', 'Yaha']
+# activated_users = []
+
+# while users:
+#     current_user = users.pop()
+#     activated_users.append(current_user)
+
+# for i in activated_users:
+#     print(i)
+#     # always put a title to look it more appealing
+#     print(i.title())
