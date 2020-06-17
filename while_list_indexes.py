ba40ab60f92ca7991418097862f045809f600870
glondhe <ganesh.londhe@crenexa.com>
2020-06-17 08:35:46 +0530

    while with list and indexes

---
+++ while_list_indexes.py
@@ -0,0 +1,10 @@
+# while with list and indexes
+
+ex_list = ['one', 'two', 'three', 'four']
+counter = 0
+max_index = len(ex_list) - 1
+
+while counter <= max_index:
+    number = ex_list[counter]
+    print(number)
+    counter += 1
