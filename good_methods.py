1314006c326d49db90f6b3352edcf239451353df
glondhe <ganesh.londhe@crenexa.com>
2020-06-29 15:25:39 +0530

    few good methods

---
+++ good_methods.py
@@ -0,0 +1,12 @@
+# some useful methods of python
+
+random_string = '234'
+
+# print(random_string.upper())
+# print(random_string.lower())
+# print(random_string.isalpha())
+# print(random_string.isalnum())
+# print(random_string.isnumeric())
+# print(random_string.isdecimal())
+# print(random_string.isspace())
+# print(random_string.istitle())
