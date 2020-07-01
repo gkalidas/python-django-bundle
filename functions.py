66c136312a7e271414d5bd8d579e2a973ae425fe
glondhe <ganesh.londhe@crenexa.com>
2020-07-01 22:31:01 +0530

    functions intro

---
+++ functions.py
@@ -0,0 +1,24 @@
+def add_numbers(a, b):
+    c = a + b
+    print("The answer is ", c)
+
+
+def subtract_numbers(a, b):
+    c = a - b
+    print("The answer is ", c)
+
+
+def multiply_numbers(a, b):
+    c = a * b
+    print("The answer is ", c)
+
+
+def divide_numbers(a, b):
+    c = a / b
+    print("The answer is ", c)
+
+
+add_numbers(243 + 45, 43)
+subtract_numbers(98, 54)
+multiply_numbers(34, 2)
+divide_numbers(12, 4)
