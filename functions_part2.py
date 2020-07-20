c57b777d6789165e80cbdbb5532d5feb3f0711a5
glondhe <ganesh.londhe@crenexa.com>
2020-07-20 08:55:18 +0530

    This is same as part one ;)

---
+++ functions_part2.py
@@ -0,0 +1,10 @@
+# default parameter in python
+
+
+def Test(my_car, car_color='white'):
+    """Display information about cars"""
+    print("I have a", my_car, "car.")
+    print(my_car, "is ", car_color, " colored.")
+
+
+Test('audi', 'blue')
