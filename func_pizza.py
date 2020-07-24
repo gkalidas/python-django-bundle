33b27f451934a26890fe191cd531df66758ffbe9
glondhe <ganesh.londhe@crenexa.com>
2020-07-24 18:45:46 +0530

    comment added

--- func_pizza.py
+++ func_pizza.py
@@ -2,6 +2,7 @@
 
 
 def make_pizza(size, *toppings):
+    """Adding topping/s to the pizze ;) """
     print('\nmaking pizza with size ' + str(size))
     for topping in toppings:
         print('_ ', topping.title())
