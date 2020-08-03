8f0dc251d106fd8dc74c5c862084a77d2011795a
glondhe <ganesh.londhe@crenexa.com>
2020-08-03 07:30:13 +0530

    comment added

--- file-intro.py
+++ file-intro.py
@@ -1,8 +1,11 @@
+# write to the file
+
 f = open('test.txt', 'w')
 content = "Writing file now has been changed."
 f.write(content)
 f.close()
 
+# read from the file
 f = open('test.txt', 'r')
 content = f.read()
 print("file content.\n", content)
