d23b9b17744326c84a0aa4d848cc7f2f77e72570
glondhe <ganesh.londhe@crenexa.com>
2020-08-05 10:41:03 +0530

    reading file with argv done, it's fun ba**

---
+++ 64_2_read_file_with_argv.py
@@ -0,0 +1,23 @@
+
+# !/usr/bin/env python3
+# # This isn't working for me geting Permissin denied error
+from sys import argv
+
+script_file, file_name = argv
+
+
+def read_file(file_name):
+    txt = open(file_name)
+    print(f'This is your file {file_name}')
+    print(txt.read())
+
+
+read_file(file_name)
+
+while True:
+    read_again = input('Would you like to read another file? (y/n) ')
+    if read_again == 'y':
+        file_name = input('Enter file name ')
+        read_file(file_name)
+    else:
+        break
